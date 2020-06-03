9a95682799dde6c984685d5fdf2746a931c2a5a9
GMalikov <lemartis@gmail.com>
2020-06-03 21:01:20 +0300

    Initial commit

---
+++ src/main/java/com/example/Template.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Template {
+
+    private Map<String, String> variables;
+
+    private String templateText;
+
+    public Template(String templateText) {
+        this.variables = new HashMap<>();
+        this.templateText = templateText;
+    }
+
+    public void set(String name, String value) {
+        this.variables.put(name, value);
+    }
+
+    public String evaluate() {
+        String result = templateText;
+        for (Map.Entry<String, String> entry: variables.entrySet()){
+            String regex = "\\$\\{" + entry.getKey() + "\\}";
+            result = result.replaceAll(regex, entry.getValue());
+        }
+        return result;
+    }
+}
---
+++ src/test/java/TestTemplate.java
@@ -0,0 +1,33 @@
+import com.example.Template;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestTemplate {
+
+    private Template template;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        template = new Template("${one}, ${two}, ${three}");
+        template.set("one", "1");
+        template.set("two", "2");
+        template.set("three", "3");
+    }
+
+    @Test
+    public void multipleVariables() throws Exception {
+        assertTemplateEvaluatesTo("1, 2, 3");
+    }
+
+    @Test
+    public void unknownVariablesAreIgnored() throws Exception {
+        template.set("doesnotexist", "whatever");
+        assertTemplateEvaluatesTo("1, 2, 3");
+    }
+
+    private void assertTemplateEvaluatesTo(String expected) {
+        Assertions.assertEquals(expected, template.evaluate());
+    }
+}
